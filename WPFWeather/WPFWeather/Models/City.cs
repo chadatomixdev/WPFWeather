949f58f435db5986ee1d4858d2afac92f90edc9c
Chad Bonthuys <chad@atomixdev.com>
2020-04-27 10:06:32 +0100

    Adding Models to be used for the API calls

---
+++ WPFWeather/WPFWeather/Models/City.cs
@@ -0,0 +1,18 @@
+﻿namespace WPFWeather.Models
+{
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
+    }
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+}
