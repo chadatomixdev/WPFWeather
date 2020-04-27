8a494700b45a85094448fd3a1c73e4920752269d
Chad Bonthuys <chad@atomixdev.com>
2020-04-27 15:48:56 +0100

    Adding the View model and ICommand Binding

---
+++ WPFWeather/WPFWeather/Commands/SearchCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+using WPFWeather.ViewModels;
+
+namespace WPFWeather.Commands
+{
+    public class SearchCommand : ICommand
+    {
+        public WeatherViewModel WeatherViewModel { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public SearchCommand(WeatherViewModel weatherViewModel)
+        {
+            WeatherViewModel = weatherViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            WeatherViewModel.MakeQueryAsync();
+        }
+    }
+}
---
+++ WPFWeather/WPFWeather/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using WPFWeather.Commands;
+using WPFWeather.Helpers;
+using WPFWeather.Models;
+
+namespace WPFWeather.ViewModels
+{
+    public class WeatherViewModel : INotifyPropertyChanged
+    {
+        public WeatherViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                selectedCity = new City() { LocalizedName = "New York" };
+                currentConditions = new CurrentConditions()
+                {
+                    WeatherText = "Partly Cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 21
+                        }
+                    }
+
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set { 
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set { 
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async void MakeQueryAsync()
+        {
+            var cities = await AccuweatherHelper.GetCitiesAsync(Query);
+        }
+    }
+}
